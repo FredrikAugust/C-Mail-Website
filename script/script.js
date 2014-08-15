1f350c982c5fece8dd6f5d1fe6ed793a00d7dcea
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-08-16 00:58:22 +0200

    Made the buttons redirect to download and project on GitHub

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#download").on("click", function () {
+        window.location = "https://github.com/Tijndagamer/C-Mail/releases";
+    });
+    $("#github").on("click", function () {
+        window.location = "https://github.com/Tijndagamer/C-Mail";
+    });
+});
