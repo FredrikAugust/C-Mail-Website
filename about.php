04deac52c84afc94535f4cfc482b682a31879ee8
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-08-27 21:42:38 +0200

    Added head to all pages

--- about.php
+++ about.php
@@ -1,13 +1,10 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<title>C-Mail</title>
-		<meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
-        <link href="css/style.css" rel="stylesheet" type="text/css">
-	</head>
+	<!-- Head -->
+	<?php
+        $title = "C-Mail - About";
+        include('parts/head.php');
+    ?>
 	
 	<body>
    	    <!-- Header -->
