6947621ae3347cabc10be71a96b2c3995d387abc
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-08-27 21:36:12 +0200

    Added scripts part

--- index.php
+++ index.php
@@ -90,10 +90,5 @@
         <?php
             include('parts/footer.php');
         ?>
-    	
-    	<!-- Scripts -->
-    	<script src="script/jquery-1.11.1.min.js" type="text/javascript"></script>
-        <script src="script/bootstrap.min.js" type="text/javascript"></script>
-        <script src="script/script.js" type="text/javascript"></script>
 	</body>
 </html>
---
+++ parts/scripts.php
@@ -0,0 +1,4 @@
+        <!-- Scripts -->
+    	<script src="script/jquery-1.11.1.min.js" type="text/javascript"></script>
+        <script src="script/bootstrap.min.js" type="text/javascript"></script>
+        <script src="script/script.js" type="text/javascript"></script>
