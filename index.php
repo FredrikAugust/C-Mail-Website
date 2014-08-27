46f675b7ce37134da0cd3b0471bf841b0842d2df
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-08-27 21:39:56 +0200

    Added head php file

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<title>C-Mail</title>
-		<meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
-        <link href="css/style.css" rel="stylesheet" type="text/css">
-	</head>
+	
 	
 	<body>
        <!-- Header -->
