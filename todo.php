ead5be1de92d48438263f4409e7dff0e5adb3ace
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-08-27 21:38:16 +0200

    Added scripts to all sites

--- todo.php
+++ todo.php
@@ -97,8 +97,8 @@
         ?>
     	
     	<!-- Scripts -->
-    	<script src="script/jquery-1.11.1.min.js" type="text/javascript"></script>
-        <script src="script/bootstrap.min.js" type="text/javascript"></script>
-        <script src="script/script.js" type="text/javascript"></script>
+        <?php
+            include('parts/scripts.php');
+        ?>
 	</body>
 </html>
