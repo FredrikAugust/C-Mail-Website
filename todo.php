f8513378e33156bc8a8487182504c5f145ede332
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2014-08-27 21:32:39 +0200

    Replaced footer on all pages

--- todo.php
+++ todo.php
@@ -92,34 +92,9 @@
         </div>
         
         <!-- Footer -->
-    	<div class="footer">
-    		<hr>
-    		<div class="container">
-    		    <div class="row">
-    		        <div class="col-sm-4 hidden-xs">
-    		            <h4>Fork the website on GitHub <br>and make it your own</h4>
-    		            <p>Did you find something you don't like? Go ahead an fork it on GitHub and make it your own!</p>
-    		            <button type="button"><a href="https://github.com/FredrikAugust/C-Mail-Website/fork"><img class="forkicon" src="img/fork.png">Fork on GitHub</a></button>
-    		        </div>
-    		        <div class="col-sm-4">
-    		            <h4>Stay connected with MrMadsenMalmo</h4>
-    		            <p>Always be the first to recieve updates on project and other things. You can also chat with me if you need help.</p>
-    		            <div class="icons">
-    		                <a href="https://dribbble.com/MrMadsenMalmo"><img src="img/dribbble.png"></a>
-    		                <a href="https://facebook.com/fredrik.madsenmalmo"><img src="img/facebook.png"></a>
-    		                <a href="https://plus.google.com/+FredrikAugustMadsenMalmo"><img src="img/g+.png"></a>
-    		                <a href="https://github.com/FredrikAugust"><img src="img/Github.png"></a>
-    		                <a href="http://twitter.com/MrMadsenMalmo"><img src="img/twitter.png"></a>
-    		            </div>
-    		        </div>
-    		        <div class="col-sm-4 hidden-xs">
-    		            <h4>Short description of <br>the project</h4>
-    		            <p>This website is dedicated to the program C-Mail made by my good friend MrTijn. <a href="https://github.com/Tijndagamer">Follow him on GitHub</a> to recieve the latest updates.</p>
-    		            <button type="button"><a href="about.html">Read More</a></button>
-    		        </div>
-    		    </div>
-    		</div>
-    	</div>
+        <?php
+            include('parts/footer.php');
+        ?>
     	
     	<!-- Scripts -->
     	<script src="script/jquery-1.11.1.min.js" type="text/javascript"></script>
